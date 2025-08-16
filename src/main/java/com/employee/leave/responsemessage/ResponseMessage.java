626412cb61a8a781b0720bc87c3bd2165a4b9cc8
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-08-16 23:44:04 +0530

    Update ResponseMessage.java

--- src/main/java/com/employee/leave/responsemessage/ResponseMessage.java
+++ src/main/java/com/employee/leave/responsemessage/ResponseMessage.java
@@ -1,7 +1,6 @@
 package com.employee.leave.responsemessage;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
