f78307997518ddab8dbbe29269c5a208ff4f45e0
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-08-16 23:43:26 +0530

    Update InvalidPasswordException.java

--- src/main/java/com/employee/leave/global/exceptions/InvalidPasswordException.java
+++ src/main/java/com/employee/leave/global/exceptions/InvalidPasswordException.java
@@ -1,8 +1,6 @@
 package com.employee.leave.global.exceptions;
 
 public class InvalidPasswordException extends RuntimeException {
-
-
 	public InvalidPasswordException(String message) {
 		super(message);
 	}
