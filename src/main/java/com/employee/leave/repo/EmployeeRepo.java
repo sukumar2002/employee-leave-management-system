a124fc9d1f31ade5c9f93ba91132b939addefe84
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-11-03 14:46:20 +0530

    Update EmployeeRepo.java

--- src/main/java/com/employee/leave/repo/EmployeeRepo.java
+++ src/main/java/com/employee/leave/repo/EmployeeRepo.java
@@ -1,13 +1,11 @@
 package com.employee.leave.repo;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.employee.leave.model.Employee;
-
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 	@Query("SELECT e FROM Employee e WHERE e.email = :email")
